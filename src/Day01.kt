89303da60c615e2d5bd191eb8ac77638e1705bd8
Maja Gacesa <maja.gacesa@gmail.com>
2024-12-01 21:26:05 +0100

    [day01] learned about `groupingBy` and `eachCount` today <3

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,20 +3,17 @@ import kotlin.math.abs
 fun main() {
 
     val rawInput = readInput("01")
+
     val leftAndRight = rawInput
         .map { text -> text.split("   ")
                 .map { it.toInt() } }
-        .associate { it.first() to it.last() }
-    // this works under the assumption there are no duplicate values in first column,
-    // which is true (but not true for the example input)
-    // question is there a way to obtain the same result with non-unique first-column values?
-    val part1 = leftAndRight.keys
-        .sorted()
-        .zip(leftAndRight.values.sorted())
+        .map { it.first() to it.last() }
+
+    val part1 = (leftAndRight.map { it.first }.sorted() zip leftAndRight.map { it.second }.sorted())
         .sumOf { abs(it.first - it.second) }
 
-    val part2 = leftAndRight.keys
-        .sumOf { left -> left * leftAndRight.values.count{right -> right == left}  }
+    val counts = leftAndRight.map { it.second }.groupingBy { it }.eachCount()
+    val part2 = leftAndRight.map{ it.first }.sumOf{ it * (counts[it] ?: 0) }
 
     println("Part 1 solution is $part1")
     println("Part 2 solution is $part2")
