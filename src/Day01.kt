7021e93da1c977db4f856e5ac2a4a345cf3883c8
Maja Gacesa <maja.gacesa@gmail.com>
2024-12-01 20:57:26 +0100

    [day01] misuse mapOf to have two "lists" but I am not happy with this

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,10 +3,21 @@ import kotlin.math.abs
 fun main() {
 
     val rawInput = readInput("01")
-    val first = rawInput.map { it.split("  ").first().toInt() }.sorted()
-    val second = rawInput.map{it.split("   ").last().toInt()}.sorted()
-    val part1 = (first zip second).sumOf { abs(it.second - it.first) }
-    val part2 = first.sumOf { inn -> inn * second.count { it -> it == inn } }
+    val leftAndRight = rawInput
+        .map { text -> text.split("   ")
+                .map { it.toInt() } }
+        .associate { it.first() to it.last() }
+    // this works under the assumption there are no duplicate values in first column,
+    // which is true (but not true for the example input)
+    // question is there a way to obtain the same result with non-unique first-column values?
+    val part1 = leftAndRight.keys
+        .sorted()
+        .zip(leftAndRight.values.sorted())
+        .sumOf { abs(it.first - it.second) }
+
+    val part2 = leftAndRight.keys
+        .sumOf { left -> left * leftAndRight.values.count{right -> right == left}  }
+
     println("Part 1 solution is $part1")
     println("Part 2 solution is $part2")
 
