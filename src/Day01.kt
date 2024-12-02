26e35ee8f011fa983f04baa6adba16d9676c66fb
Maja Gacesa <maja.gacesa@gmail.com>
2024-12-02 09:11:43 +0100

    add pre-commit hooks to ktlint everything

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,22 @@
 import kotlin.math.abs
 
 fun main() {
-
     val rawInput = readInput("01")
 
-    val leftAndRight = rawInput
-        .map { text -> text.split("   ")
-                .map { it.toInt() } }
-        .map { it.first() to it.last() }
+    val leftAndRight =
+        rawInput
+            .map { text ->
+                text
+                    .split("   ")
+                    .map { it.toInt() }
+            }.map { it.first() to it.last() }
 
-    val part1 = (leftAndRight.map { it.first }.sorted() zip leftAndRight.map { it.second }.sorted())
-        .sumOf { abs(it.first - it.second) }
+    val part1 =
+        (leftAndRight.map { it.first }.sorted() zip leftAndRight.map { it.second }.sorted())
+            .sumOf { abs(it.first - it.second) }
 
     val counts = leftAndRight.map { it.second }.groupingBy { it }.eachCount()
-    val part2 = leftAndRight.map{ it.first }.sumOf{ it * (counts[it] ?: 0) }
+    val part2 = leftAndRight.map { it.first }.sumOf { it * (counts[it] ?: 0) }
 
     println("Part 1 solution is $part1")
     println("Part 2 solution is $part2")
