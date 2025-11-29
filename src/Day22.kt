41c2c451eb266563b62a60c02f6f9ff86938b8e5
Maja Gacesa <maja.gacesa@gmail.com>
2025-11-30 00:46:46 +0100

    [day22] pt2 - how do I put only the first occurence of a sequence into the map?
    I used a hack to reverse a list of pairs but there must be a better way
    for sure!

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,19 +1,58 @@
+import kotlin.collections.map
+
 fun main() {
     val numbers = readInput("22")
             .takeWhile { it.isNotEmpty() }
             .map { it.toLong() }
     
-    var pt1 = numbers
-        for (i in 1..2000) {
+    val sequences = numbers
+        .map { secretNumber ->
+            val sequence = mutableListOf(secretNumber)
+            var newNumber = secretNumber
+            (1 .. 2000).forEach{ _ -> 
+                newNumber = newNumber.transform()
+                sequence.add(newNumber)
+            }
+            sequence
+    }
 
-           pt1 = pt1
-                .map { secretNumber -> ((secretNumber * 64) xor secretNumber) % 16777216 }
-                .map { secretNumber -> (secretNumber.div(32) xor secretNumber) % 16777216 }
-                .map { secretNumber -> ((secretNumber * 2048) xor secretNumber) % 16777216 }
-        }
-    val part1 = pt1.sum()
+
+    val part1 = sequences.sumOf { it.last() }
+
+    val prices = sequences.map {sequence -> sequence.map{it % 10}}
+
+    val sequenceToPriceMap = prices.map{ 
+        priceList -> 
+        val diffs = priceList
+            .zipWithNext()
+            .map{it.second - it.first}
+        diffs.windowed(4).zip(priceList.slice(4 until priceList.size))
+            .reversed() // TAKE THE FIRST OCCURENCE OF A SEQUENCE (is there a better way..?)
+            .associate { it }
+    }
+
+    val part2 = sequenceToPriceMap
+        // for each possible sequence
+        .flatMap { it.keys }.toSet()
+        .maxOf { sequence ->
+            sequenceToPriceMap
+                // find matching price in buyers list (if exists)
+                .mapNotNull { it[sequence] }
+                // add all the prices
+                .sum()
+    } // find max per sequence
+        .toInt()
 
     println("Part 1 solution is $part1")
+   
+    println("Part 1 solution is $part2") 
     assert(part1 == 16999668565)
+    assert(part2 == 1898)
+}
+
+private fun Long.transform(): Long {
+    val first = ((this * 64) xor this) % 16777216
+    val second = (first.div(32) xor first) % 16777216
+    return  ((second * 2048) xor second) % 16777216
 }
 
